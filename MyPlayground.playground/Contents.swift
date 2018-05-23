357a0c9b848154d62eaa6b3cb2b4a0316a85d31e
kleinerQ <chih.yen5408@gmail.com>
2018-05-23 10:32:09 +0800

    1805231010

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,189 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+
+//let str = "ABCDEF    \n \n"
+//let str2 = "Hello, playground"
+//let arr = Array(str2)
+//print(arr[5])
+////print(str2[2]) // not allowed
+//str2[.init(encodedOffset: 7)]
+//str2[str2.index(str.startIndex, offsetBy: 5)]
+//
+//str2.prefix(5)
+//str2.suffix(5)
+//let begin = str2.index(of: "p")
+//let end = str2.index(begin!, offsetBy:4)
+//let newStr = String(str2[begin!..<end])
+//if(str2.contains("pl")){
+//    print("AA")
+//}
+//let tmp = str2.replacingOccurrences(of: "play", with: "Haloo")
+//
+//
+//if(str2.hasPrefix("He")){
+//    print("The string start from He")
+//}
+//
+//if(str2.hasSuffix("ound")){
+//    print("The string end for ound")
+//}
+//
+//let s = "wang, Lily Braun weg,0931-1212123"
+//let list = s.split(separator: ",")
+//var arr3 = [3,5,1,8,2]
+//arr3.sorted();
+//arr3
+//
+//arr3.sorted(by: >)
+//
+//let x = arr3.reversed()
+//x.forEach{(element) in
+//    print(element)
+//}
+//
+////索引值1 3 互換
+//arr3.swapAt(1, 3)
+//
+//arr3.first
+//arr3.last
+//arr3.count
+//arr3[1]
+
+//str.count;
+//
+//let indexStart = str.index(of:"C")
+//let indexEnd = str.index(of:"E")
+//var subStr = str[indexStart!..<indexEnd!]
+//
+//print(subStr)
+//
+//var tmp=str.trimmingCharacters(in: .whitespacesAndNewlines)
+//
+//
+//tmp.lowercased()
+//
+//tmp.uppercased()
+//
+//for i in 1...10 where i % 2 == 1{
+//    print(i)
+//}
+//
+//let cityArray=["台北","台中","高雄"]
+//for city in cityArray{
+//    print(city)
+//}
+
+//for i in (0 ..< 10).reversed() {
+//
+//    print(i)
+//}
+//
+//
+//
+//for index in stride(from: 5, to: 1, by: -1) {
+//    print(index)
+//}
+////prints 5, 4, 3, 2
+//
+//for index in stride(from: 5, through: 1, by: -1) {
+//    print(index)
+//}
+////prints 5, 4, 3, 2, 1
+
+
+//
+//
+//var n=0
+//while n<10{
+//
+//    print(n)
+//    n+=2
+//}
+
+
+
+//
+//var n2=0
+//repeat {
+//
+//    print(n2)
+//    n2+=2
+//}while n2<10
+//
+//let arr=Array(repeating: "a", count: 20)
+//for element in arr{
+//    print(element)
+//}
+//
+//let arr2=Array(repeating: Array(repeating: 0, count: 8), count: 3)
+//for element in arr2{
+//    print(element)
+//}
+//
+//
+//var name = ["andy", "Mary","Tom"]
+//name.append("Don")
+//name.insert("Jolin", at: 2)
+//
+//
+//var arr3 = [5,7,2,4]
+//arr3 = [5,2,31]
+//arr3.sorted().forEach { (n) in
+//
+//    print(n)
+//
+//}
+//
+//for n in arr3.sorted(){
+//
+//    print(n)
+//}
+//
+//let arr4=arr3.sorted();
+//for idx in 0 ..< arr4.count{
+//    print(arr4[idx])
+//}
+//
+//var color: [UIColor:String] = [:]
+//color[UIColor.black] = "黑色"
+//color[UIColor.yellow] = "黃色"
+//color[UIColor.red] = "紅色"
+//print(color[UIColor.red]!)
+//
+//var age=["F":30, "B":40, "C":50]
+//
+//let tmp2 = age.sorted{ (d1, d2) -> Bool in
+//
+//
+//    return d1.key < d2.key
+//
+//
+//}
+//
+//print(tmp2)
+//
+//let a=10;
+//let b=2;
+//
+//if a==10,b==20{
+//    print("AAA")
+//
+//}else{
+//
+//    print("BBB")
+//
+//}
+
+
+
+
+//var aa = "10"
+//var nn : Int!
+//nn=Int(aa)
+//print(nn * 10)
+
+var dict: [String:String] = [:]
